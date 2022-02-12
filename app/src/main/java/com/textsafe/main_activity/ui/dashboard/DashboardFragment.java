9f79ce1fb18006c2cbf00edf0c5500dc8f6c8732
Angela <tiangela1@gmail.com>
2022-02-12 13:43:32 -0800

    Modified login UI layout

--- app/src/main/java/com/textsafe/main_activity/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/textsafe/main_activity/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,66 @@
 package com.textsafe.main_activity.ui.dashboard;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.provider.ContactsContract;
+
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
+import com.textsafe.main_activity.R;
 import com.textsafe.main_activity.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment
+    implements LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener{
 
     private FragmentDashboardBinding binding;
+    /*
+     * Defines an array that contains column names to move from
+     * the Cursor to the ListView.
+     */
+    @SuppressLint("InlinedApi")
+    private final static String[] FROM_COLUMNS = {
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                    ContactsContract.Contacts.DISPLAY_NAME
+    };
+    /*
+     * Defines an array that contains resource ids for the layout views
+     * that get the Cursor column contents. The id is pre-defined in
+     * the Android framework, so it is prefaced with "android.R.id"
+     */
+    private final static int[] TO_IDS = {
+            android.R.id.text1
+    };
+    // Define global mutable variables
+    // Define a ListView object
+    ListView contactsList;
+    // Define variables for the contact the user selects
+    // The contact's _ID value
+    long contactId;
+    // The contact's LOOKUP_KEY
+    String contactKey;
+    // A content URI for the selected contact
+    Uri contactUri;
+    // An adapter that binds the result Cursor to the ListView
+    private SimpleCursorAdapter cursorAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,12 +72,48 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+//        return root;
+
+        contactsList =
+                (ListView) root.findViewById(R.id.contact_list_view);
+        // Gets a CursorAdapter
+        cursorAdapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.contacts_list_item,
+                null,
+                FROM_COLUMNS, TO_IDS,
+                0);
+        // Sets the adapter for the ListView
+        contactsList.setAdapter(cursorAdapter);
+
+        return inflater.inflate(R.layout.fragment_dashboard,
+                container, false);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        
     }
 }
